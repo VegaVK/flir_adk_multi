2da2be0da96d8a2edea0cf1675c88d9c9c65091d
Vamsi Vegamoor <44810844+VegaVK@users.noreply.github.com>
2021-04-12 22:05:52 -0500

    script to extract images for training

---
+++ scripts/extractImages.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#Modified: April 12, 2021: VVK
+# Extract images from rosbag files into .jpeg for training. Bag file names and
+#   export paths are hardcoded.
+import rospy
+import std_msgs
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from sensor_msgs.msg import Image
+import numpy as np
+
+def main():
+    rospy.init_node('image_extractor', anonymous=True)
+    extractor=image_extractor()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+class image_extractor:
+
+    def __init__(self):
+        self.bridge=CvBridge()
+        rospy.Subscriber('/Thermal_Panorama', Image, self.extract)
+        self.fileCounter=1
+
+    ## FOR EACH IMAGE:
+    # def extract(self,data):
+    #     imageFromBag=self.bridge.imgmsg_to_cv2(data, "mono8")
+    #     # The export path is hardcoded:
+    #     print(self.fileCounter)
+    #     cv2.imwrite('/home/vamsi/RainDataTrain/bag13/'+str(self.fileCounter)+'.jpeg',imageFromBag)
+    #     self.fileCounter=self.fileCounter+1
+    
+    ## FOR ALTERNATE IMAGES:
+    def extract(self,data):
+        imageFromBag=self.bridge.imgmsg_to_cv2(data, "mono8")
+        # The export path is hardcoded:
+        if  (self.fileCounter % 2) == 0:
+            print(self.fileCounter)
+            cv2.imwrite('/home/vamsi/RainDataTrain/bag11/'+str(self.fileCounter)+'.jpeg',imageFromBag)
+            self.fileCounter=self.fileCounter+1
+        else:
+            self.fileCounter=self.fileCounter+1
+
+
+
+
+    
+if __name__=='__main__':
+    main()
+#######################
+
